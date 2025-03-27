a08c40e636654d94ca2d8bd374d84d994cb47df1
WWYDF <89052946+WWYDF@users.noreply.github.com>
2025-03-27 01:24:50 -0400

    Fix post links

--- src/components/clientSide/Posts/PostCard.tsx
+++ src/components/clientSide/Posts/PostCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Posts } from "@prisma/client";
+import Link from "next/link";
 
 
 interface PostCardProps {
@@ -12,9 +13,27 @@ export default function PostCard({ post, viewMode }: PostCardProps) {
   const thumbnailSrc = `/thumbnails/image/small_${post.id}.webp`;
 
   return (
-    <div className="rounded-xl bg-secondary-border overflow-hidden">
-      {viewMode === 'GRID' ? (
-        <div className="aspect-square">
+    <Link href={`/post/${post.id}`} className="block group">
+      <div className="rounded-xl bg-secondary-border overflow-hidden">
+        {viewMode === 'GRID' ? (
+          <div className="aspect-square">
+            <picture>
+              <source
+                srcSet={`/thumbnails/image/large_${post.id}.webp`}
+                media="(min-width: 1024px)"
+              />
+              <source
+                srcSet={`/thumbnails/image/medium_${post.id}.webp`}
+                media="(min-width: 640px)"
+              />
+              <img
+                src={`/thumbnails/image/small_${post.id}.webp`}
+                alt={post.fileName}
+                className="w-full h-full object-cover"
+              />
+            </picture>
+          </div>
+        ) : (
           <picture>
             <source
               srcSet={`/thumbnails/image/large_${post.id}.webp`}
@@ -27,35 +46,19 @@ export default function PostCard({ post, viewMode }: PostCardProps) {
             <img
               src={`/thumbnails/image/small_${post.id}.webp`}
               alt={post.fileName}
-              className="w-full h-full object-cover"
+              className="w-full object-cover"
             />
           </picture>
+        )}
+
+
+        {/* Metadata like filename and timestamp */}
+        <div className="p-2 text-xs text-subtle">
+          {post.fileName}
+          <br />
+          {new Date(post.createdAt).toLocaleDateString()}
         </div>
-      ) : (
-        <picture>
-          <source
-            srcSet={`/thumbnails/image/large_${post.id}.webp`}
-            media="(min-width: 1024px)"
-          />
-          <source
-            srcSet={`/thumbnails/image/medium_${post.id}.webp`}
-            media="(min-width: 640px)"
-          />
-          <img
-            src={`/thumbnails/image/small_${post.id}.webp`}
-            alt={post.fileName}
-            className="w-full object-cover"
-          />
-        </picture>
-      )}
-
-
-      {/* Metadata like filename and timestamp */}
-      <div className="p-2 text-xs text-subtle">
-        {post.fileName}
-        <br />
-        {new Date(post.createdAt).toLocaleDateString()}
       </div>
-    </div>
+    </Link>
   );
 }
