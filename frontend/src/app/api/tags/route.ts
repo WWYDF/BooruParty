cb72f626f54ad1703e53550578b061da12d23d1e
WWYDF <89052946+WWYDF@users.noreply.github.com>
2025-03-28 01:12:41 -0400

    tag searching api

---
+++ frontend/src/app/api/tags/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/core/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const url = new URL(request.url);
+    const searchParam = url.searchParams.get('search');
+    const tag = searchParam ? String(searchParam) : undefined;
+
+    if (tag) {
+        try {
+            const prismaSearch = await prisma.tagName.findMany({
+              where: {
+                name: {
+                  contains: tag,
+                  mode: 'insensitive',
+                },
+              },
+              include: {
+                tag: {
+                  include: {
+                    names: true, // Include all names (aliases + canonical)
+                    category: true, // Optional: if you want the category too
+                  },
+                },
+              },
+            });
+              
+            return NextResponse.json(prismaSearch);
+    
+        } catch (error) {
+            return NextResponse.json({ error: `Failed to fetch tag data for '${tag}': ${error}` }, { status: 500 });
+        }
+    }
+}
