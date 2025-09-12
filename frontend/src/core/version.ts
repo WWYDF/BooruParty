772de9dd7e26175e73d7682f4b41aa71e352066f
devin <89052946+WWYDF@users.noreply.github.com>
2025-09-12 17:26:48 -0400

    Update version.ts

--- frontend/src/core/version.ts
+++ frontend/src/core/version.ts
@@ -2,4 +2,4 @@
 // Manually changing this yourself could lead to accidental file deletion.
 
 // Only developers should update this when pushing a new version.
-export const CURRENT_VERSION = '1.0.0';
+export const CURRENT_VERSION = '0.10.1';
