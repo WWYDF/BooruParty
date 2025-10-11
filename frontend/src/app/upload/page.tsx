e874b73bef3e48e29b396fe86a1b35ff7f6b78be
WWYDF <89052946+WWYDF@users.noreply.github.com>
2025-10-11 14:39:19 -0400

    Crashfix
    Prisma tabcomplete moment

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -1,6 +1,7 @@
 import UploadQueue from "@/components/clientSide/Uploads/UploadQueue";
 import { checkPermissions } from "@/components/serverSide/permCheck";
 import { Metadata } from "next";
+import { prisma } from '@/core/prisma';
 
 const site_name = process.env.NEXT_PUBLIC_SITE_NAME || 'https://example.com'
 
@@ -29,11 +30,6 @@ export default async function UploadPage() {
   const canDupe = perms['post_create_dupes'];
   let showAutoTagButton = false;
 
-  const autoTagRules = await prisma?.addonsConfig.findFirst({ where: { id: 1 } });
-  if (autoTagRules && autoTagRules.autoTagger && autoTagRules.autoTaggerMode.includes('SELECTIVE')) {
-    showAutoTagButton = perms['post_autotag'];
-  }
-
   if (!canUpload) { 
     return (
       <main className="min-h-screen flex flex-col items-center justify-center text-center px-4 text-red-400">
@@ -41,6 +37,11 @@ export default async function UploadPage() {
         <p className="text-base text-subtle max-w-md">Sorry, but you are not allowed to create posts.</p>
       </main>
     );
+  }
+
+  const autoTagRules = await prisma.addonsConfig.findFirst({ where: { id: 1 } });
+  if (autoTagRules && autoTagRules.autoTagger && autoTagRules.autoTaggerMode.includes('SELECTIVE')) {
+    showAutoTagButton = perms['post_autotag'];
   }
 
   return (
