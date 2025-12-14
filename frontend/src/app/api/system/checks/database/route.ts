570e64a2587ed9c668ca8b4f9416e94d22b51dbd
WWYDF <89052946+WWYDF@users.noreply.github.com>
2025-12-13 19:02:39 -0500

    added auth key to system checks

--- frontend/src/app/api/system/checks/database/route.ts
+++ frontend/src/app/api/system/checks/database/route.ts
@@ -163,7 +163,8 @@ async function fixPreviewSizes() {
       method: 'POST',
       body: JSON.stringify(body),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'x-internal-secret': process.env.INTERNAL_SHARED_SECRET!
       }
     });
 
