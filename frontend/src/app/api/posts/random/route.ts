72ea942bd17da8a2b8eeafa69d0b69a94f3d3337
WWYDF <89052946+WWYDF@users.noreply.github.com>
2026-02-15 03:06:45 -0500

    added type filtering to random search

--- frontend/src/app/api/posts/random/route.ts
+++ frontend/src/app/api/posts/random/route.ts
@@ -2,16 +2,31 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/core/prisma';
 import { auth } from '@/core/authServer';
 import { checkPermissions } from '@/components/serverSide/permCheck';
+import { FILE_TYPE_MAP, FileType } from '@/core/dictionary';
 
-export async function GET() {
+export async function GET(req: Request) {
   const session = await auth();
   const hasPerms = (await checkPermissions(['post_view']))['post_view'];
   if (!session || !hasPerms) { return NextResponse.json({ error: "Unauthorized" }, { status: 401 }); };
 
+  const { searchParams } = new URL(req.url);
+  const postType = searchParams.get("type") || "";
+  if (postType && !["image", "animated", "video", "other"].includes(postType)) { return NextResponse.json({ error: "Invalid Post Type" }, { status: 400 }); };
+
+  const whereClause = postType 
+    ? { 
+        fileExt: { 
+          in: FILE_TYPE_MAP[postType as FileType].map(ext => ext.replace('.', ''))
+        } 
+      }
+    : {}
+  ;
+
   try {
-    const allPosts = await prisma.posts.count();
+    const allPosts = await prisma.posts.count({ where: whereClause });
     const skip = Math.floor(Math.random() * allPosts);
     const post = await prisma.posts.findFirst({
+      where: whereClause,
       skip,
       take: 1
     });
